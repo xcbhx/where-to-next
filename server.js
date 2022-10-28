d3dc5e0fae9b98b2da654a419829604e338e74dd
xcbhx <ceina.ellison@gmail.com>
2022-10-28 14:05:32 -0400

    Connect to database

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+require('dotenv').config();
+//Connect to the MongoDB database
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
