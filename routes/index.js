0d98e4c6ef745b7b12cab233c6f5a538449f5600
xcbhx <ceina.ellison@gmail.com>
2022-10-28 18:05:21 -0400

    render landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optional
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/travels',
+    // Change to what's best for YOUR app
+    failureRedirect: '/travels'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    // Change path for your "landing" page
+    res.redirect('/travels');
+  });
 });
 
 module.exports = router;
